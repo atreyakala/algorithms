f6debac398f2599d14eeadebba207f562769d676
atreyaKala <atreya_kala@yahoo.co.in>
2020-03-24 03:00:08 +0530

    add copyRandomList

--- copyRandomList.py
+++ copyRandomList.py
@@ -1,20 +1,43 @@
-class Node:
-    def __init__(self, x, next = None, random = None):
-        self.val = int(x)
-        self.next = next
-        self.random = random
+def copyRandomList(head):
+    map = {None: None}
+    node = head
+    while node is not None:
+        copyNode = Node(node.val)
+        map[node] = copyNode
+        node = node.next
+    node = head
+    while node is not None:
+        copyNode = map[node]
+        copyNode.next = map[node.next]
+        copyNode.random = map[node.random]
+        node = node.next
+    return map[head]
 
-    def copyRandomList(self, head):
-        map = {None: None}
-        node = head
-        while node is not None:
-            copyNode = Node(node.val)
-            map[node] = copyNode
-            node = node.next
-        node = head
-        while node is not None:
-            copyNode = map[node]
-            copyNode.next = map[node.next]
-            copyNode.random = map[node.random]
-            node = node.next
-        return map[head]
+def copyRandomList(head):
+    if head is None:
+        return None
+    node = head
+    while node is not None:
+        next = node.next
+        copy = Node(node.val)
+        node.next = copy
+        copy.next = next
+        node = next
+    node = head
+    while node is not None:
+        copy = node.next
+        if node.random:
+            copy.random = node.random.next
+        node = copy.next
+    copyHead = None
+    node = head
+    while node is not None:
+        copy = node.next
+        if copyHead is None:
+            copyHead = copy
+        next = copy.next
+        node.next = copy.next
+        if copy.next is not None:
+            copy.next = copy.next.next
+        node = next
+    return copyHead
