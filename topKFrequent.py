3e813e63e272c6267f748081487eca2b7422f339
atreyaKala <atreya_kala@yahoo.co.in>
2020-03-28 15:25:36 +0530

    add bucket sort solution for topKFrequent

--- topKFrequent.py
+++ topKFrequent.py
@@ -10,3 +10,14 @@ def topKFrequent(nums, k):
         else:
             heapq.heappush(maxFreqHeap, (freq, num))
     return [heapq.heappop(maxFreqHeap)[1] for _ in range(k)][::-1]
+
+def topKFrequent(nums, k):
+    buckets = [[] for _ in range(len(nums) + 1)]
+    result = []
+    for num, freq in Counter(nums).items():
+        buckets[freq].append(num)
+    for li in reversed(buckets):
+        if len(li) > 0:
+            result += li
+        if len(result) >= k:
+            return result[:k]
