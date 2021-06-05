44ab006338f3aa2d9384394bb4c8fcc8936a9a72
Atreya Kala <atreya_kala@yahoo.co.in>
2021-06-06 00:06:32 +0530

    Add files

--- kthClosestToOrigin.py
+++ kthClosestToOrigin.py
@@ -1,11 +1,17 @@
-def kClosest(points, k):
+import heapq
+from typing import List
+
+
+def k_closest(points: List[List[int]], k: int) -> List[List[int]]:
     heap = []
     for point in points:
         x, y = point
-        dist = x*x + y*y
-        if len(heap) < k:
-            heapq.heappush(heap, (-dist, point))
+        dist = x * x + y * y
+        if len(heap) == k:
+            heapq.heappushpop(heap, (-dist, x, y))
         else:
-            if dist < -heap[0][0]:
-                heapq.heappushpop(heap, (-dist, point))
-    return [elem[1] for elem in heap]
+            heapq.heappush(heap, (-dist, x, y))
+    return [(x, y) for (_, x, y) in heap]
+
+
+# O(N * logK) | O(k)
--- productExceptSelf.py
+++ productExceptSelf.py
@@ -1,9 +1,12 @@
-def productExceptSelf(nums):
+from typing import List
+
+
+def product_except_self(nums: List[int]) -> int:
     ans = [1] * len(nums)
     for i in range(1, len(nums)):
-        ans[i] = ans[i - 1] * nums[i - 1]
-    right = 1
+        ans[i] = nums[i - 1] * ans[i - 1]
+    right_running_product = 1
     for i in reversed(range(len(nums))):
-        ans[i] *= right
-        right *= nums[i]
+        ans[i] *= right_running_product
+        right_running_product *= nums[i]
     return ans
