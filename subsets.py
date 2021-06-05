6ed58857ecf12836eccc5631b73e4641c2dc0854
Atreya Kala <atreya_kala@yahoo.co.in>
2021-06-06 00:34:17 +0530

    Add subsets

--- subsets.py
+++ subsets.py
@@ -1,13 +1,35 @@
-def subsets(nums):
-    powerset = [[]]
-    for num in nums:
-        powerset += [subset + [num] for subset in powerset]
-    return powerset
+"""
+78. Subsets https://leetcode.com/problems/subsets/
 
-def powerset(array):
+Given an integer array nums of unique elements, return all possible subsets (the power set).
+The solution set must not contain duplicate subsets. Return the solution in any order.
+
+Example 1:
+Input: nums = [1, 2, 3]
+Output: [[], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3]]
+
+Example 2:
+Input: nums = [0]
+Output: [[], [0]]
+
+Constraints:
+1 <= nums.length <= 10
+-10 <= nums[i] <= 10
+All the numbers of nums are unique.
+"""
+
+from typing import List
+
+
+def powerset(arr: List[int]) -> List[List[int]]:
     subsets = [[]]
-    for ele in array:
-        for i in xrange(len(subsets)):
-            currentSubset = subset[i]
-            subsets.append(currentSubset)
+    for ele in arr:
+        subsets += [subset + [ele] for subset in subsets]
     return subsets
+
+
+def test_0():
+    assert powerset([1, 2]) == [[], [1], [2], [1, 2]]
+
+
+# O(N * 2^N) | O(N * 2^N)
